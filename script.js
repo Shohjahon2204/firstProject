f1b7e0bdc09dff41351536f3c365f6885f63dc06
Shohjahon <shohjahon2204@gmail.com>
2020-08-11 11:22:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/*jshint esversion: 6 */
+/*let number = 5;
+const leftBoderWidth = 1;
+number = 10;
+console.log(number);
+const obj = {
+a: 50
+};
+obj.a = 10;
+console.log(obj);
+
+
+var name = 'Ivan';
+
+{
+    var ss = 4;
+}
+
+console.log(ss);
+
+const result = confirm("are you here?");
+
+
+const answer = prompt("are you 18?", "");
+console.log(typeof(answer));*/
+
+/*const answers = [];
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("What is your surename?", "");
+answers[2] = prompt("What is your address?", "");
+answers[3] = prompt("What is your age?", "");
+
+console.log(answers);*/
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
